ccd3ba6b42262143dad658364389a3a8908de511
Mike <miguelandin@proton.me>
2026-04-13 15:32:59 +0200

    FEAT: schemas done

---
+++ intermedia/src/models/user.model.js
@@ -0,0 +1,91 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Mail not valid"]
+        },
+        password: {
+            type: String,
+            required: [true, 'Contraseña requerida'],
+            minLength: [8, 'Mínimo 8 caracteres'],
+            select: false
+        },
+        name: {
+            type: String,
+            required: [true, 'Nombre requerido']
+        },
+        lastName: {
+            type: String
+        },
+        nif: {
+            type: String,
+            required: [true, 'NIF requerido']
+        },
+        role: {
+            type: String,
+            enum: {
+                values: ['admin', 'guest'],
+                message: 'El rol debe de ser admin o guest'
+            },
+            default: 'admin'
+        },
+        status: {
+            type: String,
+            enum: ['pending', 'verified'],
+            default: 'pending',
+            index: true
+        },
+        verificationCode: {
+            type: String,
+            default: null,
+            select: false,
+            minlength: 6,
+            maxlength: 6
+        },
+        verificationAttempts: {
+            type: Number,
+            default: 0,
+            max: [3, 'Maximo número de intentos']
+        },
+        company: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Company',
+            index: true
+        },
+        address: {
+            street: {
+                type: String
+            },
+            number: {
+                type: String
+            }
+            , postal: {
+                type: String
+            },
+            city: {
+                type: String
+            },
+            province: {
+                type: String
+            }
+        },
+        deleted: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+userSchema.virtual('fullName').get(function() {
+    return `${this.name} ${this.lastName || ''}`.trim();
+});
+
+export default mongoose.model('User', userSchema)
